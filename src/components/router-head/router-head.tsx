71815027127a67ae3773f5c9bc14eeb858408f9e
Timo Anttila <code@tuspe.com>
2023-01-17 23:08:56 +0200

    Testing static build
    Signed-off-by: Timo Anttila <code@tuspe.com>

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,38 +1,32 @@
-import {component$} from '@builder.io/qwik'
-import {useDocumentHead, useLocation} from '@builder.io/qwik-city'
+import { component$ } from '@builder.io/qwik';
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-	const head = useDocumentHead()
-	const loc = useLocation()
+  const head = useDocumentHead();
+  const loc = useLocation();
 
-	return (
-		<>
-			<title>{head.title}</title>
-			<link rel="canonical" href={loc.href} />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-			<link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-			<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-			<link rel="manifest" href="/site.webmanifest" />
-			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-			<meta name="msapplication-TileColor" content="#00aba9" />
-			<meta name="theme-color" content="#ffffff" />
+  return (
+    <>
+      <title>{head.title}</title>
 
-			{head.meta.map(m => (
-				<meta {...m} />
-			))}
+      <link rel="canonical" href={loc.href} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
-			{head.links.map(l => (
-				<link {...l} />
-			))}
+      {head.meta.map((m) => (
+        <meta {...m} />
+      ))}
 
-			{head.styles.map(s => (
-				<style {...s.props} dangerouslySetInnerHTML={s.style} />
-			))}
-		</>
-	)
-})
+      {head.links.map((l) => (
+        <link {...l} />
+      ))}
+
+      {head.styles.map((s) => (
+        <style {...s.props} dangerouslySetInnerHTML={s.style} />
+      ))}
+    </>
+  );
+});
